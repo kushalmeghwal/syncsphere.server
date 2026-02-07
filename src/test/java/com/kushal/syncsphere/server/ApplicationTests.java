c5a48c11b70bca8543ca25cc11d20e3f16cd8814
kushal Meghwal <kushalmeghwal4@gmail.com>
2026-02-07 15:11:18 +0530

    pushed everything

---
+++ src/test/java/com/kushal/syncsphere/server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kushal.syncsphere.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
